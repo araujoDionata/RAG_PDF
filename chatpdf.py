064c636ad8c23cf959562f50b08a464e50d26aa2
martins <araujo.dionata@gmail.com>
2024-08-07 20:28:16 -0300

    Update chatpdf.py

--- chatpdf.py
+++ chatpdf.py
@@ -27,7 +27,7 @@ def load_prompt():
 
 def load_llm():
     from langchain_openai import ChatOpenAI
-    llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+    llm = ChatOpenAI(model_name="gpt-4o", temperature=0.1)
     return llm
 
 def format_docs(docs):
